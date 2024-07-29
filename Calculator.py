fa40abee9f3a67b16134d9a312725fc7b9027078
challayasaswi <150769620+challayasaswi@users.noreply.github.com>
2024-07-29 19:17:17 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,52 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Error! Division by zero is not allowed."
+    else:
+        return x / y
+
+def calculator():
+    print("Welcome to the Simple Calculator!")
+    while True:
+        print("\nSelect operation:")
+        print("1.Addition")
+        print("2.Subtraction")
+        print("3.Multiplication")
+        print("4.Division")
+        print("5.Exit")
+        
+        choice = input("Enter choice( 1 / 2 / 3 / 4 / 5 ): ")
+        
+        if choice in ('1', '2', '3', '4'):
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+            
+            if choice == '1':
+                print(num1, "+", num2, "=", add(num1, num2))
+                
+            elif choice == '2':
+                print(num1, "-", num2, "=", subtract(num1, num2))
+                
+            elif choice == '3':
+                print(num1, "*", num2, "=", multiply(num1, num2))
+                
+            elif choice == '4':
+                print(num1, "/", num2, "=", divide(num1, num2))
+                
+        elif choice == '5':
+            print("Hurray! Thanks for using the Simple Calculator.")
+            break
+            
+        else:
+            print("Invalid Input . Please check once again.")
+
+if __name__ == "__main__":
+    calculator()
